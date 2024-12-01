c6a6454a6924144f6b6d4c16098a813a385f8fd0
Tiago Teixeira Santos <tiagots86@hotmail.com>
2024-12-01 20:31:07 -0300

    correções

--- deployment/outputs.tf
+++ deployment/outputs.tf
@@ -1,12 +1,39 @@
-output "vpc_ssm" {
-  value = aws_ssm_parameter.vpc.name
+output "lb_external_arn" {
+  value = aws_ssm_parameter.lb_external_arn.id
 }
 
-
-output "public_subnets_ssm" {
-  value = aws_ssm_parameter.public_subnets[*].name
+output "lb_external_listener_arn" {
+  value = aws_ssm_parameter.lb_external_listener_arn.id
 }
 
-output "private_subnets_ssm" {
-  value = aws_ssm_parameter.private_subnets[*].name
+output "lb_internal_arn" {
+  value = aws_ssm_parameter.lb_internal_arn.id
+}
+
+output "lb_internal_listener_arn" {
+  value = aws_ssm_parameter.lb_internal_listener_arn.id
+}
+
+output "service_discovery_cloudmap_name" {
+  value = aws_ssm_parameter.service_discovery_cloudmap_name.id
+}
+
+output "service_discovery_cloudmap_id" {
+  value = aws_ssm_parameter.service_discovery_cloudmap_id.id
+}
+
+output "service_discovery_service_connect_name" {
+  value = aws_ssm_parameter.service_discovery_service_connect_name.id
+}
+
+output "service_discovery_service_connect_id" {
+  value = aws_ssm_parameter.service_discovery_service_connect_id.id
+}
+
+output "vpc_link" {
+  value = aws_ssm_parameter.vpc_link.id
+}
+
+output "vpc_link_arn" {
+  value = aws_ssm_parameter.vpc_link_arn.id
 }
